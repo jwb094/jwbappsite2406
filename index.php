3dcb538e46571b0bc937fd83b2da04df01e45fd1
Josh Bello <josh.bello@thirtythree.co.uk>
2024-07-08 15:07:30 +0100

    update(index.php) - download-now component/section with utility classes & (projectstyle & c_download ) projectstyle.css updated via changes in c_download_now partial file

--- index.php
+++ index.php
@@ -105,17 +105,20 @@
             <section data-component-download-now class="pt-64 pb-64" id="download-now">
                 <div class="l-container">
                     <div class="row">
-                        <div class="col-xs-12 col-md-offset-4 col-md-8 ">
-                            <h2 class="mb-24"><?= $sectionTitle2 ?></h2>
+                        <div class="col-xs-12  col-md-8">
+                            <h1 class="mb-24"><?= $sectionTitle2 ?></h1>
                             <div class=" flex flex-col  justify-content-between | mb-24 ">
-                                <p class="mb-24"><?= $contextLg ?>
+                                <p class="leading-180 | mb-24"><?= $contextLg ?>
                                 </p>
-                                <p class="mb-24"><?= $contextLg ?>
+                                <p class="leading-180 | mb-24"><?= $contextLg ?>
                                 </p>
                             </div>
 
-                            <a class="mt-32 mb-24" href="#">CTA</a>
+                            <a class="border-4 border-white border-solid  px-96 py-12 w-6-12 mt-32 mb-24" href="#">Download Now</a>
                         </div>
+                        <!-- <div class="hidden md.flex col-md-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" fill="currentColor" class="bi bi-display" viewBox="0 0 16 16"> <path d="M0 4s0-2 2-2h12s2 0 2 2v6s0 2-2 2h-4c0 .667.083 1.167.25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75c.167-.333.25-.833.25-1.5H2s-2 0-2-2V4zm1.398-.855a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3H2c-.325 0-.502.078-.602.145z"/> </svg>
+                        </div> -->
                     </div>
                 </div>
             </section>
